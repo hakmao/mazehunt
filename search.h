bcee3b9204237ba8f5c4c5b464672d766cb17a9c
Johannes Flieger <j.flieger@gmail.com>
2019-12-30 03:08:18 +0000

    Remove previous implementation of search

--- search.h
+++ search.h
@@ -9,7 +9,10 @@
 using std::deque;
 
 // directional deltas
-const int delta[4][2]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+const int delta[4][2]{{ -1, 0 },
+					  { 0,  -1 },
+					  { 1,  0 },
+					  { 0,  1 }};
 
 struct CandidateCell
 {
@@ -17,32 +20,43 @@ struct CandidateCell
 	int y;
 	int g;
 	int h;
-	CandidateCell(int a, int b, int c, int d): x{a}, y{b}, g{c}, h{d}{}; // Needed for emplace_back()
+
+	CandidateCell(int a, int b, int c, int d) : x{ a }, y{ b }, g{ c }, h{ d }
+	{
+	}; // for use with emplace_back()
 };
 
-bool compare_cells(const CandidateCell& a, const CandidateCell& b);
+bool compare_cells(CandidateCell const & a, CandidateCell const & b);
 
 class PathFinder
 {
 private:
 	// data
 //	Cell goal;
-	deque<Cell> path{};
-	vector<CandidateCell> openList{};
+	deque <Cell> path{};
+	vector <CandidateCell> openList{};
 	GridLayout grid{};
 
 	// members
 	bool is_valid_cell(int x, int y);
+
 	int calculate_heuristic(int x, int y, Cell g);
+
 	void sort_open_list();
+
 	void add_to_open_list(int x, int y, int g, int h);
-	void expand_neighbours(const CandidateCell &current, Cell goal);
+
+	void expand_neighbours(const CandidateCell& current, Cell goal);
 
 public:
 	PathFinder(GridLayout grd);
+
 	bool search(Cell init, Cell goal);
-	deque<Cell> get_path();
+
+	deque <Cell> get_path();
+
 	void print_path();
+
 	void print_open_list();
 
 };
