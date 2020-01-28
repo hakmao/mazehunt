e2a530264e2c6641d3dad1aa2f50c7c121927507
Johannes Flieger <j.flieger@gmail.com>
2020-01-28 20:15:30 +0000

    Change demo for Grid class to handle initialisation from vector input as well as text files

--- grid_demo.cpp
+++ grid_demo.cpp
@@ -6,15 +6,17 @@
 
 int main()
 {
-	std::cout << "\n=== Create new Grid object from grid state vector ===\n";
-	Grid grid1(sample_grid_1);
-	std::cout << "\n=== Create another Grid object ===\n";
-	Grid grid2(sample_grid_2);
+	std::cout << "\n=== Create first Grid object from grid state vector ===\n";
+	Grid grid1(grid_tests::sample_grid_1);
+	std::cout << "\n=== Create second Grid object ===\n";
+	Grid grid2(grid_tests::sample_grid_2);
+    std::cout << "\n=== Create third Grid object (using same vector as the first) ===\n";
+    Grid grid3(grid_tests::sample_grid_1);
 	//
-	std::cout << "\n=== Check if new Grid object is equal to first one (expect true) ===\n" << std::endl;
+	std::cout << "\n=== Check that second Grid object is NOT equal to first one (expect false) ===\n" << std::endl;
 	std::cout << ((grid1 == grid2) ? "- true" : "- false") << std::endl;
-	std::cout << "\n=== Check if new Grid object is NOT equal to first one (expect false) ===\n" << std::endl;
-	std::cout << ((grid1 != grid2) ? "- true" : "- false") << std::endl;
+    std::cout << "\n=== Check that first Grid object is equal to the third one (expect true) ===\n" << std::endl;
+    std::cout << ((grid1 == grid3) ? "- true" : "- false") << std::endl;
 	std::cout << "\n=== Export representation of grid layout to file ===\n" << std::endl;
 	if (grid1.to_file(grid_tests::testfile))
 	{
@@ -26,8 +28,8 @@ int main()
 	}
 	std::cout << "\n=== Create Grid object from file, and print to stdout ===\n"
 			  << std::endl;
-	Grid grid3(grid_tests::testfile);
-	std::cout << grid3 << std::endl;
+	Grid grid4(grid_tests::testfile);
+	std::cout << grid4 << std::endl;
 
 	return 0;
 }
